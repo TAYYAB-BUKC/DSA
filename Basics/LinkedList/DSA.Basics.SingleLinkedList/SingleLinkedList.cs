7c84a21cfde4b0f3a1059bb8dfbe6308ac260892
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-11-05 23:37:06 +0500

    DisplayList method implemented in SingleLinkedList.

---
+++ Basics/LinkedList/DSA.Basics.SingleLinkedList/SingleLinkedList.cs
@@ -0,0 +1,24 @@
+﻿namespace DSA.Basics.SingleLinkedList
+{
+	public class SingleLinkedList
+	{
+		private Node start { get; set; } = null!;
+		
+		public void DisplayList()
+		{
+			if(start is null)
+			{
+				Console.WriteLine("LinkedList is empty.");
+				return;
+			}
+
+			Console.WriteLine("LinkedList Items");
+			Node node = start;
+			while(node != null)
+			{
+				Console.Write($"{node.info}\t");
+				node = node.link;
+			}
+		}
+	}
+}
