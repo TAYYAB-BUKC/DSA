712e81dbbabb2a466afc713f3ac353b95dbf66f3
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-11-06 12:20:35 +0500

    Hardcoded list initialization removed.

--- Basics/LinkedList/DSA.Basics.SingleLinkedList/SingleLinkedList.cs
+++ Basics/LinkedList/DSA.Basics.SingleLinkedList/SingleLinkedList.cs
@@ -4,24 +4,6 @@
 	{
 		private Node start { get; set; } = null!;
 
-		public SingleLinkedList()
-		{
-			Node first = new Node(10);
-			Node second = new Node(20);
-			Node third = new Node(30);
-			Node fourth = new Node(40);
-			Node fifth = new Node(50);
-
-			// Linking nodes
-			first.link = second;
-			second.link = third;
-			third.link = fourth;
-			fourth.link = fifth;
-
-			// Assign head
-			start = first;
-		}
-
 		public void DisplayList()
 		{
 			if(start is null)
