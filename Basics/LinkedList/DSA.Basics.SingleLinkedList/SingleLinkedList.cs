6670c0a0f32ced72028576667d7ac356110aba82
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-11-06 12:23:06 +0500

    CreateList method implemented.

--- Basics/LinkedList/DSA.Basics.SingleLinkedList/SingleLinkedList.cs
+++ Basics/LinkedList/DSA.Basics.SingleLinkedList/SingleLinkedList.cs
@@ -94,5 +94,13 @@
 				node.link = newNode;
 			}
 		}
+
+		public void CreateList(int numberOfNodes)
+		{
+			for (int i = 0; i < numberOfNodes; i++)
+			{
+				InsertAtEnd(new Random().Next(i, 1000));
+			}
+		}
 	}
 }
