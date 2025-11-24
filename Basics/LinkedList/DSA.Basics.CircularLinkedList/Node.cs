19a0aed44d1f7c144c48b3cae9e24f65f2beae64
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-11-24 21:22:43 +0500

    Node class added.

---
+++ Basics/LinkedList/DSA.Basics.CircularLinkedList/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace DSA.Basics.CircularLinkedList
+{
+	public class Node
+	{
+		public int info { get; set; }
+		public Node link { get; set; }
+
+		public Node(int info)
+		{
+			this.info = info;
+			this.link = null!;
+		}
+	}
+}
