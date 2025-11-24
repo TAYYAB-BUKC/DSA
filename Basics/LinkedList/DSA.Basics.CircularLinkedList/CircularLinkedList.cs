4cd7b05b8ebd2a6ba5f006482d94f045030a61f3
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-11-24 21:30:26 +0500

    DisplayList method implemented.

---
+++ Basics/LinkedList/DSA.Basics.CircularLinkedList/CircularLinkedList.cs
@@ -0,0 +1,25 @@
+﻿namespace DSA.Basics.CircularLinkedList
+{
+	public class CircularLinkedList
+	{
+		public Node end { get; set; } = null!;
+
+		public void DisplayList()
+		{
+			if(end is null)
+			{
+				Console.WriteLine("List is empty");
+				return;
+			}
+
+			Node node = end.link;
+			do
+			{
+				Console.WriteLine($"{node.info}\t");
+				node = node.link;
+			}
+			while (node != end.link);
+			Console.WriteLine();
+		}
+	}
+}
