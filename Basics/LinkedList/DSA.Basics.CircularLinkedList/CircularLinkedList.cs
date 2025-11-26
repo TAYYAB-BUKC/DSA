b901f168f0327ea0b776d7086b7d75afe4560f92
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-11-26 23:27:58 +0500

    CreateList method implemented in CircularLinkedList.

--- Basics/LinkedList/DSA.Basics.CircularLinkedList/CircularLinkedList.cs
+++ Basics/LinkedList/DSA.Basics.CircularLinkedList/CircularLinkedList.cs
@@ -15,6 +15,8 @@
 			Node node = end.link;
 			do
 			{
+				if (node is null)
+					break;
 				Console.WriteLine($"{node.info}\t");
 				node = node.link;
 			}
@@ -34,6 +36,29 @@
 
 			newNode.link = end.link;
 			end.link = newNode;
+		}
+
+		public void InsertAtEnd(int info)
+		{
+			Node newNode = new Node(info);
+			if (end is null)
+			{
+				end = newNode;
+				end.link = newNode;
+				return;
+			}
+
+			newNode.link = end.link;
+			end.link = newNode;
+			end = newNode;
+		}
+
+		public void CreateList(int numberOfNodes)
+		{
+			for (int i = 0; i < numberOfNodes; i++)
+			{
+				InsertAtEnd(new Random().Next(i, 1000));
+			}
 		}
 	}
 }
