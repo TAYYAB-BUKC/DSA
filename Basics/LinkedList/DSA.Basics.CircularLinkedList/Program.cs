5d4d208257928c630af150c546647c7b3125b455
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-11-28 21:04:19 +0500

    Method tested.

--- Basics/LinkedList/DSA.Basics.CircularLinkedList/Program.cs
+++ Basics/LinkedList/DSA.Basics.CircularLinkedList/Program.cs
@@ -7,15 +7,16 @@ CircularLinkedList list = new CircularLinkedList();
 
 list.CreateList(5);
 list.InsertAtBeginning(5);
-list.InsertAtBeginning(15);
+//list.InsertAtBeginning(15);
 //list.InsertAtBeginning(15);
 //list.InsertAtBeginning(20);
 //list.InsertAtBeginning(25);
 //list.InsertAtBeginning(30);
-//list.InsertAtEnd(35);
-//list.InsertAtEnd(40);
-list.InsertAfter(5, 10);
-
+list.InsertAtEnd(35);
+list.InsertAtEnd(40);
+list.InsertAfter(40, 100);
+list.DisplayList();
+list.DeleteFirstNode();
 list.DisplayList();
 
 Console.ReadKey();
