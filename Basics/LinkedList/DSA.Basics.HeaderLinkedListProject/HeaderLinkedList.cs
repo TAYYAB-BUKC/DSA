77a83adcf7be21d497194f31569ca712eff917c3
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-01 23:15:33 +0500

    DisplayList method implemented in HeaderLinkedList.

---
+++ Basics/LinkedList/DSA.Basics.HeaderLinkedListProject/HeaderLinkedList.cs
@@ -0,0 +1,25 @@
+﻿namespace DSA.Basics.HeaderLinkedListProject
+{
+	public class HeaderLinkedList
+	{
+		public Node head { get; set; } = null!;
+
+		public void DisplayList()
+		{
+			if(head.link is null)
+			{
+				Console.WriteLine("List is empty\n");
+				return;
+			}
+
+			Node node = head.link;
+			Console.Write("Current List:\n");
+			while (node != null)
+			{
+				Console.Write($"{node.info}\t");
+				node = node.link;
+			}
+			Console.WriteLine();
+		}
+	}
+}
