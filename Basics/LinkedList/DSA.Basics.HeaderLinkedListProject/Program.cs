fd060e34e1faa4035536eca35e2ef7ef9c2ea4d9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-01 23:27:15 +0500

    Method tested.

--- Basics/LinkedList/DSA.Basics.HeaderLinkedListProject/Program.cs
+++ Basics/LinkedList/DSA.Basics.HeaderLinkedListProject/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using DSA.Basics.HeaderLinkedListProject;
+
+HeaderLinkedList list = new HeaderLinkedList();
+
+list.InsertAtBeginning(20);
+list.InsertAtBeginning(15);
+list.InsertAtBeginning(10);
+list.InsertAtBeginning(5);
+
+list.DisplayList();
+
+Console.ReadKey();
