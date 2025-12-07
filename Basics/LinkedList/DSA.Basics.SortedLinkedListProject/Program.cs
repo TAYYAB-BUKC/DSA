8259f028158d3b435d3332ae6ff2d63de5f31f8d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-07 20:50:08 +0500

    Method tested.

--- Basics/LinkedList/DSA.Basics.SortedLinkedListProject/Program.cs
+++ Basics/LinkedList/DSA.Basics.SortedLinkedListProject/Program.cs
@@ -1,23 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DSA.Basics.SortedLinkedListProject;
 
-int numberOfNodes, data;
+//int numberOfNodes, data;
 
 SortedLinkedList list = new();
 
 list.DisplayList();
 
-Console.Write("Enter the number of nodes : ");
-numberOfNodes = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Enter the number of nodes : ");
+//numberOfNodes = Convert.ToInt32(Console.ReadLine());
 
-if (numberOfNodes == 0)
-	return;
+//if (numberOfNodes == 0)
+//	return;
 
-for (int i = 1; i <= numberOfNodes; i++)
-{
-	Console.Write("Enter the element to be inserted : ");
-	data = Convert.ToInt32(Console.ReadLine());
-	list.InsertInOrder(data);
-}
+//for (int i = 1; i <= numberOfNodes; i++)
+//{
+//	Console.Write("Enter the element to be inserted : ");
+//	data = Convert.ToInt32(Console.ReadLine());
+//	list.InsertInOrder(data);
+//}
+
+list.CreateList(5);
+
+list.DisplayList();
+
+list.CreateList(5);
 
 list.DisplayList();
