697e62aa39146c788d372e200819cbb6222b3394
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-07 20:36:37 +0500

    InsertInOrder method implemented in SortedLinkedList.

--- Basics/LinkedList/DSA.Basics.SortedLinkedListProject/SortedLinkedList.cs
+++ Basics/LinkedList/DSA.Basics.SortedLinkedListProject/SortedLinkedList.cs
@@ -9,5 +9,26 @@
 			start = null!;
 		}
 
+		public void InsertInOrder(int data)
+		{
+			Node newNode = new Node(data);
+
+			/*List empty or new node to be inserted before first node*/
+			if (start == null || data < start.info)
+			{
+				newNode.link = start;
+				start = newNode;
+				return;
+			}
+
+			Node node = start;
+			while (node.link != null && node.link.info <= data)
+			{
+				node = node.link;
+			}
+			
+			newNode.link = node.link;
+			node.link = newNode;
+		}
 	}
 }
