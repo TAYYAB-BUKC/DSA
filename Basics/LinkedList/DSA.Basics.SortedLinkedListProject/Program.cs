cff04233b3dde401ff1b2c9e8a739bd268529762
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-09 00:10:09 +0500

    User Menu Implemented.

--- Basics/LinkedList/DSA.Basics.SortedLinkedListProject/Program.cs
+++ Basics/LinkedList/DSA.Basics.SortedLinkedListProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using DSA.Basics.SortedLinkedListProject;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
 
 int choice, info;
 
@@ -18,6 +20,26 @@ while (true)
 
 	switch (choice)
 	{
-		
+		case 0:
+			Console.WriteLine("Enter the number of elements to be inserted : ");
+			info = Convert.ToInt32(Console.ReadLine());
+			list.CreateList(info);
+			break;
+		case 1:
+			list.DisplayList();
+			break;
+		case 2:
+			Console.WriteLine("Enter the element to be inserted : ");
+			info = Convert.ToInt32(Console.ReadLine());
+			list.InsertInOrder(info);
+			break;
+		case 3:
+			Console.WriteLine("Enter the element to be searched : ");
+			info = Convert.ToInt32(Console.ReadLine());
+			list.Search(info);
+			break;
+		default:
+			Console.WriteLine("Wrong choice\n");
+			break;
 	}
 }
