444a8541fedd6303604f2f2e976beaaacebd65d3
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-09 00:05:48 +0500

    User Menu Added.

--- Basics/LinkedList/DSA.Basics.SortedLinkedListProject/Program.cs
+++ Basics/LinkedList/DSA.Basics.SortedLinkedListProject/Program.cs
@@ -1,41 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DSA.Basics.SortedLinkedListProject;
+﻿using DSA.Basics.SortedLinkedListProject;
 
-//int numberOfNodes, data;
+int choice, info;
 
-SortedLinkedList list = new();
+SortedLinkedList list = new SortedLinkedList();
 
-list.DisplayList();
+while (true)
+{
+	Console.WriteLine("0. Create List");
+	Console.WriteLine("1. Display List");
+	Console.WriteLine("2. Insert a new node");
+	Console.WriteLine("3. Search");
+	Console.WriteLine("4. Exit");
+	Console.Write("Enter your choice : ");
+	choice = Convert.ToInt32(Console.ReadLine());
+	if (choice == 4)
+		break;
 
-list.InsertInOrder(0);
-list.InsertInOrder(5);
-list.InsertInOrder(10);
-list.InsertInOrder(15);
-list.InsertInOrder(20);
-
-//Console.Write("Enter the number of nodes : ");
-//numberOfNodes = Convert.ToInt32(Console.ReadLine());
-
-//if (numberOfNodes == 0)
-//	return;
-
-//for (int i = 1; i <= numberOfNodes; i++)
-//{
-//	Console.Write("Enter the element to be inserted : ");
-//	data = Convert.ToInt32(Console.ReadLine());
-//	list.InsertInOrder(data);
-//}
-
-list.CreateList(5);
-
-list.DisplayList();
-
-list.CreateList(5);
-
-list.DisplayList();
-
-list.Search(100);
-list.Search(5);
-list.Search(-5);
-list.Search(20);
-
+	switch (choice)
+	{
+		
+	}
+}
