6b18e113be9f8637054790b804da06f3132bd7c0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-11 10:39:17 +0500

    Size and IsEmpty methods implemented in Stack in StackLinkedListProject.

---
+++ Basics/Stack/DSA.Basics.StackLinkedListProject/Stack.cs
@@ -0,0 +1,24 @@
+﻿namespace DSA.Basics.StackLinkedListProject
+{
+	public class Stack
+	{
+		public Node top { get; set; } = null!;
+
+		public int Size()
+		{
+			Node node = top;
+			int counter = 0;
+			while (node != null)
+			{
+				counter++;
+				node = node.link;
+			}
+			return counter;
+		}
+
+		public bool IsEmpty()
+		{
+			return top is null;
+		}
+	}
+}
