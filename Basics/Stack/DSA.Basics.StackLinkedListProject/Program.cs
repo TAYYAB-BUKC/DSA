c619bff602dc743cf1cd1519ee16fb9fb4eb3478
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-13 11:52:04 +0500

    User Menu Added.

--- Basics/Stack/DSA.Basics.StackLinkedListProject/Program.cs
+++ Basics/Stack/DSA.Basics.StackLinkedListProject/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.StackLinkedListProject;
+
+int choice, info;
+
+Stack stack = new();
+
+while (true)
+{
+	Console.WriteLine("1. Push an element on the stack");
+	Console.WriteLine("2. Pop an element from the stack");
+	Console.WriteLine("3. Display the top element");
+	Console.WriteLine("4. Display all stack elements");
+	Console.WriteLine("5. Display size of the stack");
+	Console.WriteLine("6. Quit");
+	Console.Write("Enter your choice : ");
+	choice = Convert.ToInt32(Console.ReadLine());
+
+	if (choice == 6)
+		break;
+
+	switch (choice)
+	{
+		
+	}
+	Console.WriteLine();
+}
