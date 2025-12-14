cb0d5dd344c7ed94002c4c3c162ca91024549134
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-14 22:00:30 +0500

    IsEmpty and IsFull methods implemented in Queue in QueueArrayProject.

--- Basics/Queue/DSA.Basics.QueueArrayProject/Queue.cs
+++ Basics/Queue/DSA.Basics.QueueArrayProject/Queue.cs
@@ -12,11 +12,22 @@
 			front = -1;
 			rear = -1;
 		}
+
 		public Queue(int size)
 		{
 			queueArray = new int[size];
 			front = -1;
 			rear = -1;
+		}
+
+		public bool IsEmpty()
+		{
+			return (front == -1 || front == rear + 1);
+		}
+
+		public bool IsFull()
+		{
+			return (rear == queueArray.Length - 1);
 		}
 	}
 }
