d4460f74954ae7b857533d1cc637d0f75e7e096f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-14 21:59:02 +0500

    Queue class added.

---
+++ Basics/Queue/DSA.Basics.QueueArrayProject/Queue.cs
@@ -0,0 +1,22 @@
+﻿namespace DSA.Basics.QueueArrayProject
+{
+	public class Queue
+	{
+		private int[] queueArray;
+		private int front;
+		private int rear;
+
+		public Queue()
+		{
+			queueArray = new int[10];
+			front = -1;
+			rear = -1;
+		}
+		public Queue(int size)
+		{
+			queueArray = new int[size];
+			front = -1;
+			rear = -1;
+		}
+	}
+}
