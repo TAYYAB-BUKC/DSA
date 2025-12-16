f43a5d68ade92ff4e3d8103b9cfd329196882ccc
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-16 23:56:59 +0500

    Queue class added.

---
+++ Basics/Queue/DSA.Basics.QueueLinkedListProject/Queue.cs
@@ -0,0 +1,14 @@
+﻿namespace DSA.Basics.QueueLinkedListProject
+{
+	public class Queue
+	{
+		private Node front;
+		private Node rear;
+
+		public Queue()
+		{
+			front = null!;
+			rear = null!;
+		}
+	}
+}
