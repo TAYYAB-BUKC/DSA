5f0d024f2615e314a57fabcc279d8b3c0670d7ee
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-21 11:01:01 +0500

    CircularQueue Using Array Class Added.

---
+++ Basics/Queue/DSA.Basics.CircularQueueArrayProject/CircularQueueUsingArray.cs
@@ -0,0 +1,23 @@
+﻿namespace DSA.Basics.CircularQueueArrayProject
+{
+	public class CircularQueueUsingArray
+	{
+		private int[] queueArray;
+		private int front;
+		private int rear;
+
+		public CircularQueueUsingArray()
+		{
+			queueArray = new int[10];
+			front = -1;
+			rear = -1;
+		}
+
+		public CircularQueueUsingArray(int maxSize)
+		{
+			queueArray = new int[maxSize];
+			front = -1;
+			rear = -1;
+		}
+	}
+}
