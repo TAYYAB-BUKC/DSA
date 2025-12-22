71904383df2ba96e51c34f3b4b78cfb10bb688f4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-22 22:51:02 +0500

    User Menu Added.

--- Basics/Queue/DSA.Basics.CircularQueueArrayProject/Program.cs
+++ Basics/Queue/DSA.Basics.CircularQueueArrayProject/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.CircularQueueArrayProject;
+
+int choice, info;
+
+CircularQueueUsingArray queue = new(8);
+
+while (true)
+{
+	Console.WriteLine("1. Insert an element in the queue");
+	Console.WriteLine("2. Delete an element from the queue");
+	Console.WriteLine("3. Display element at the front");
+	Console.WriteLine("4. Display all elements of the queue");
+	Console.WriteLine("5. Display size of the queue");
+	Console.WriteLine("6. Quit");
+	Console.Write("Enter your choice : ");
+	choice = Convert.ToInt32(Console.ReadLine());
+	if (choice == 6)
+		break;
+
+	switch (choice)
+	{
+		
+	}
+	Console.WriteLine();
+}
