2e10489cd6e08f0af123973ea866d67f2bbde267
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-23 23:31:20 +0500

    DoubleEndedQueueUsingArray Class Added.

---
+++ Basics/Queue/DSA.Basics.DoubleEndedQueueUsingArrayProject/DoubleEndedQueueUsingArray.cs
@@ -0,0 +1,23 @@
+﻿namespace DSA.Basics.DoubleEndedQueueUsingArrayProject
+{
+	public class DoubleEndedQueueUsingArray
+	{
+		private int[] queueArray;
+		private int front;
+		private int rear;
+
+		public DoubleEndedQueueUsingArray()
+		{
+			queueArray = new int[10];
+			front = -1;
+			rear = -1;
+		}
+
+		public DoubleEndedQueueUsingArray(int maxSize)
+		{
+			queueArray = new int[maxSize];
+			front = -1;
+			rear = -1;
+		}
+	}
+}
