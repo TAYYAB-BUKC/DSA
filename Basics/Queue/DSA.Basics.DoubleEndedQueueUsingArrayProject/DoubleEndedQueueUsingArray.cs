152eb80565a515c455d33dca7a86b522699e3490
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-23 23:32:49 +0500

    IsEmpty and IsFull methods implemented in DoubleEndedQueueUsingArray in DoubleEndedQueueUsingArrayProject.

--- Basics/Queue/DSA.Basics.DoubleEndedQueueUsingArrayProject/DoubleEndedQueueUsingArray.cs
+++ Basics/Queue/DSA.Basics.DoubleEndedQueueUsingArrayProject/DoubleEndedQueueUsingArray.cs
@@ -19,5 +19,15 @@
 			front = -1;
 			rear = -1;
 		}
+
+		public bool IsEmpty()
+		{
+			return front == -1;
+		}
+
+		public bool IsFull()
+		{
+			return (front == 0 && rear == queueArray.Length - 1) || (front == rear + 1);
+		}
 	}
 }
