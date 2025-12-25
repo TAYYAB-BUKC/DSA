956a70dbafbb87426a00c4a52bc5a9f9fff46ade
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-25 23:16:09 +0500

    User Menu Added.

--- Basics/Queue/DSA.Basics.DoubleEndedQueueUsingArrayProject/Program.cs
+++ Basics/Queue/DSA.Basics.DoubleEndedQueueUsingArrayProject/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.DoubleEndedQueueUsingArrayProject;
+
+int choice, info;
+
+DoubleEndedQueueUsingArray doubleEndedQueue = new(8);
+
+while (true)
+{
+	Console.WriteLine("1. Insert at the front end");
+	Console.WriteLine("2. Insert at the rear end");
+	Console.WriteLine("3. Delete from front end");
+	Console.WriteLine("4. Delete from rear end");
+	Console.WriteLine("5. Display all elements of deque");
+	Console.WriteLine("6. Quit");
+	Console.Write("Enter your choice : ");
+	choice = Convert.ToInt32(Console.ReadLine());
+
+	if (choice == 6)
+		break;
+
+	switch (choice)
+	{
+		
+	}
+}
