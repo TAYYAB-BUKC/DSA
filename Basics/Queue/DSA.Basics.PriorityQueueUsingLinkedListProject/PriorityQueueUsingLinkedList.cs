d25eea8273888eb0685e3458cd70b1f34a43d12f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-26 22:33:18 +0500

    Display method implemented in PriorityQueueUsingLinkedList in PriorityQueueUsingLinkedListProject.

--- Basics/Queue/DSA.Basics.PriorityQueueUsingLinkedListProject/PriorityQueueUsingLinkedList.cs
+++ Basics/Queue/DSA.Basics.PriorityQueueUsingLinkedListProject/PriorityQueueUsingLinkedList.cs
@@ -8,5 +8,26 @@
 		{
 			return front is null;
 		}
+
+		public void Display()
+		{
+			Node node = front;
+			if (IsEmpty())
+			{
+				Console.WriteLine("Queue is in underflow state");
+				return;
+			}
+			else
+			{
+				Console.WriteLine("Queue is :");
+				Console.WriteLine("Priority\tElement");
+				while (node != null)
+				{
+					Console.WriteLine($"{node.priority}\t{node.info}");
+					node = node.link;
+				}
+			}
+			Console.WriteLine();
+		}
 	}
 }
