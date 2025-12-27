4644535aecd5d05f916459a8cd30f91207ab2a64
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-27 14:45:23 +0500

    Insert method implemented in PriorityQueueUsingLinkedList in PriorityQueueUsingLinkedListProject.

--- Basics/Queue/DSA.Basics.PriorityQueueUsingLinkedListProject/PriorityQueueUsingLinkedList.cs
+++ Basics/Queue/DSA.Basics.PriorityQueueUsingLinkedListProject/PriorityQueueUsingLinkedList.cs
@@ -29,5 +29,28 @@
 			}
 			Console.WriteLine();
 		}
+
+		public void Insert(int element, int priority)
+		{
+			Node newNode, node;
+
+			newNode = new Node(element, priority);
+
+			/*Queue is empty or element to be added has priority more than first element*/
+			if (IsEmpty() || priority < front.priority)
+			{
+				newNode.link = front;
+				front = newNode;
+			}
+			else
+			{
+				node = front;
+				while (node.link != null && node.link.priority <= priority)
+					node = node.link;
+
+				newNode.link = node.link;
+				node.link = newNode;
+			}
+		}
 	}
 }
