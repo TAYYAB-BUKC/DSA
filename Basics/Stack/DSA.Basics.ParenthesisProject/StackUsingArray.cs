540e869e9b8fc537746d8297af46cd7635afd312
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-28 22:11:23 +0500

    StackUsingArray class added.

---
+++ Basics/Stack/DSA.Basics.ParenthesisProject/StackUsingArray.cs
@@ -0,0 +1,20 @@
+﻿namespace DSA.Basics.ParenthesisProject
+{
+	public class StackUsingArray
+	{
+		private int[] stackArray;
+		private int top;
+
+		public StackUsingArray()
+		{
+			stackArray = new int[10];
+			top = -1;
+		}
+
+		public StackUsingArray(int maxSize)
+		{
+			stackArray = new int[maxSize];
+			top = -1;
+		}
+	}
+}
