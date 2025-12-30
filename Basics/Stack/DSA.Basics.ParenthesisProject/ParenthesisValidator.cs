86df1a8c8e5b7c65a2c244c26bfc47c6359ab0ac
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-30 23:32:57 +0500

    IsValid method implemented in ParenthesisValidator in ParenthesisProject.

--- Basics/Stack/DSA.Basics.ParenthesisProject/ParenthesisValidator.cs
+++ Basics/Stack/DSA.Basics.ParenthesisProject/ParenthesisValidator.cs
@@ -12,5 +12,49 @@
 				return true;
 			return false;
 		}
+
+		static bool IsValid(string expression)
+		{
+			StackUsingArray stack = new();
+
+			char? character;
+
+			for (int i = 0; i < expression.Length; i++)
+			{
+				if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[')
+					stack.Push(expression[i]);
+
+				if (expression[i] == ')' || expression[i] == '}' || expression[i] == ']')
+					if (stack.IsEmpty())
+					{
+						Console.WriteLine("Right parentheses are more than left parentheses");
+						return false;
+					}
+					else
+					{
+						character = stack.Pop();
+						if (character.HasValue)
+						{
+							if (!AreParenthesisMatch(character.Value, expression[i]))
+							{
+								Console.WriteLine("Mismatched parentheses are : ");
+								Console.WriteLine(character + " and " + expression[i]);
+								return false;
+							}
+						}
+					}
+			}
+
+			if (stack.IsEmpty())
+			{
+				Console.WriteLine("Balanced Parentheses");
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("Left parentheses are more than right parentheses");
+				return false;
+			}
+		}
 	}
 }
