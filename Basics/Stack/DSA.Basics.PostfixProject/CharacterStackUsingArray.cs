be2d8589f1cac2fb1063922476cb9545d8b625a1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-12-31 23:56:07 +0500

    Size, IsEmpty and IsFull methods implemented in CharacterStackUsingArray in PostfixProject.

---
+++ Basics/Stack/DSA.Basics.PostfixProject/CharacterStackUsingArray.cs
@@ -0,0 +1,35 @@
+﻿namespace DSA.Basics.PostfixProject
+{
+	public class CharacterStackUsingArray
+	{
+		private char[] stackArray;
+		private int top;
+
+		public CharacterStackUsingArray()
+		{
+			stackArray = new char[10];
+			top = -1;
+		}
+
+		public CharacterStackUsingArray(int maxSize)
+		{
+			stackArray = new char[maxSize];
+			top = -1;
+		}
+
+		public int Size()
+		{
+			return top + 1;
+		}
+
+		public bool IsEmpty()
+		{
+			return top == -1;
+		}
+
+		public bool IsFull()
+		{
+			return top == stackArray.Length - 1;
+		}
+	}
+}
