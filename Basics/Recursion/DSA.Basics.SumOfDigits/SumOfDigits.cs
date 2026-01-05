7a587111526489ee7b91041fdaa50c1c2129e33c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-05 22:59:07 +0500

    CalculateSumOfDigits method implemented in SumOfDigits in SumOfDigitsProject.

---
+++ Basics/Recursion/DSA.Basics.SumOfDigits/SumOfDigits.cs
@@ -0,0 +1,13 @@
+﻿namespace DSA.Basics.SumOfDigits
+{
+    public class SumOfDigits
+    {
+        public static int CalculateSumOfDigits(int number)
+        {
+            if (number / 10 == 0)
+                return number;
+
+            return CalculateSumOfDigits(number / 10) + number % 10;
+        }
+    }
+}
