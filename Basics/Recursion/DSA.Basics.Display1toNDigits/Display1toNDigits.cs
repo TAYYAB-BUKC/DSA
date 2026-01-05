3079476302e5e2dfbec3e4e52f5d6420b6b50f66
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-05 23:41:27 +0500

    Display1toNDigitsInDescendingOrder method implemented in Display1toNDigits in Display1toNDigitsProject.

--- Basics/Recursion/DSA.Basics.Display1toNDigits/Display1toNDigits.cs
+++ Basics/Recursion/DSA.Basics.Display1toNDigits/Display1toNDigits.cs
@@ -6,8 +6,18 @@
         {
             if (number == 0)
                 return;
+
             Display1toNDigitsInAscendingOrder(number - 1);
             Console.Write(number + " ");
+        }
+
+        public static void Display1toNDigitsInDescendingOrder(int number)
+        {
+            if (number == 0)
+                return;
+
+            Console.Write(number + " ");
+            Display1toNDigitsInDescendingOrder(number - 1);
         }
     }
 }
