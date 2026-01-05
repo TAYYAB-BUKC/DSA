a639b944be65425f7e7c85220413d22a825025c7
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-05 23:42:07 +0500

    Testing method implemented in Program in Display1toNDigitsProject.

--- Basics/Recursion/DSA.Basics.Display1toNDigits/Program.cs
+++ Basics/Recursion/DSA.Basics.Display1toNDigits/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.Display1toNDigits;
+
+Console.WriteLine("Enter value of n : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Display1toNDigits.Display1toNDigitsInAscendingOrder(number);
+
+Console.WriteLine();
+Display1toNDigits.Display1toNDigitsInDescendingOrder(number);
+Console.WriteLine();
+
+Console.ReadKey();
