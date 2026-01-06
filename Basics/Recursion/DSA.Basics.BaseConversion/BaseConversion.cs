a738af3eb19ba6ac5551cea36df5606e10eb52b0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-06 23:06:26 +0500

    ConvertBaseToBinary method implemented in BaseConversion in BaseConversionProject.

---
+++ Basics/Recursion/DSA.Basics.BaseConversion/BaseConversion.cs
@@ -0,0 +1,14 @@
+﻿namespace DSA.Basics.BaseConversion
+{
+    public class BaseConversion
+    {
+        public static void ConvertBaseToBinary(int number)
+        {
+            if (number == 0)
+                return;
+
+            ConvertBaseToBinary(number / 2);
+            Console.Write(number % 2);
+        }
+    }
+}
