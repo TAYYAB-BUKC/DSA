6dad1bb2391644fdf5ee20aabc756402514cba51
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-06 23:10:23 +0500

    ConvertBase method implemented in BaseConversion in BaseConversionProject.

--- Basics/Recursion/DSA.Basics.BaseConversion/BaseConversion.cs
+++ Basics/Recursion/DSA.Basics.BaseConversion/BaseConversion.cs
@@ -10,5 +10,20 @@
             ConvertBaseToBinary(number / 2);
             Console.Write(number % 2);
         }
+
+        public static void ConvertBase(int number, int baseValue)
+        {
+            if (number == 0)
+                return;
+            
+            ConvertBase(number / baseValue, baseValue);
+            
+            int remainder = number % baseValue;
+
+            if (remainder < 10)
+                Console.Write(remainder);
+            else
+                Console.Write((char)(remainder - 10 + 'A'));
+        }
     }
 }
