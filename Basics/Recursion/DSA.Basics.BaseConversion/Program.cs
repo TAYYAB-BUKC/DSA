5845b2315645e72546190e7d42bc9336d795eceb
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-06 23:15:58 +0500

    Testing method implemented in Program in BaseConversionProject.

--- Basics/Recursion/DSA.Basics.BaseConversion/Program.cs
+++ Basics/Recursion/DSA.Basics.BaseConversion/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.BaseConversion;
+
+Console.Write("Enter a positive decimal number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Binary form : "); BaseConversion.ConvertBaseToBinary(number);
+Console.WriteLine();
+Console.Write("Binary form : "); BaseConversion.ConvertBase(number, 2);
+Console.WriteLine();
+Console.Write("Octal form : "); BaseConversion.ConvertBase(number, 8);
+Console.WriteLine();
+Console.Write("Hexadecimal form : "); BaseConversion.ConvertBase(number, 16);
+Console.WriteLine();
