d20ac51dea1e67c008377da8ec64cbd5451ed9e1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-07 23:55:56 +0500

    Power method implemented in Exponentiation in ExponentiationProject.

---
+++ Basics/Recursion/DSA.Basics.Exponentiation/Exponentiation.cs
@@ -0,0 +1,13 @@
+﻿namespace DSA.Basics.Exponentiation
+{
+    public class Exponentiation
+    {
+        public static double Power(double baseValue, int number)
+        {
+            if (number == 0)
+                return 1;
+
+            return baseValue * Power(baseValue, number - 1);
+        }
+    }
+}
