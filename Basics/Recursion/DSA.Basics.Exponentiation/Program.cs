afec7cc37b28b18c12efbe229bf7ca45e8249816
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-07 23:58:11 +0500

    Testing method implemented in Exponentiation in ExponentiationProject.

--- Basics/Recursion/DSA.Basics.Exponentiation/Program.cs
+++ Basics/Recursion/DSA.Basics.Exponentiation/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.Exponentiation;
+
+Console.Write("Enter values for number and baseValue : ");
+
+double number = Convert.ToDouble(Console.ReadLine());
+
+int baseValue = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number + "^" + baseValue + "=" + Exponentiation.Power(number, baseValue));
