96bf1a4621f1e4919898e49b31b8c8a7a4c8c804
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-08 22:57:25 +0500

    Testing method implemented in EuclideanAlgorithm in EuclideanAlgorithmProject.

--- Basics/Recursion/DSA.Basics.EuclideanAlgorithm/Program.cs
+++ Basics/Recursion/DSA.Basics.EuclideanAlgorithm/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.EuclideanAlgorithm;
+
+Console.Write("Enter values for firstNumber and secondNumber : ");
+long firstNumber = Convert.ToInt64(Console.ReadLine());
+long secondNumber = Convert.ToInt64(Console.ReadLine());
+
+Console.WriteLine("Greatest Common Divisor of " + firstNumber + " and " + secondNumber + " is " + EuclideanAlgorithm.CalculateGreatestCommonDivisor(firstNumber, secondNumber));
