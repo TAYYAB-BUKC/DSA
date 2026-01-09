dea69e465d5507eb6c4fcbbf2379ccb3c3d4cfc1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-09 23:48:59 +0500

    CalculateFibonacciSeries method implemented in Fibonacci in FibonacciProject.

---
+++ Basics/Recursion/DSA.Basics.Fibonacci/Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿namespace DSA.Basics.Fibonacci
+{
+    public class Fibonacci
+    {
+        public static int CalculateFibonacciSeries(int number)
+        {
+            if (number == 0)
+                return 0;
+
+            if (number == 1)
+                return 1;
+
+            return CalculateFibonacciSeries(number - 1) + CalculateFibonacciSeries(number - 2);
+        }
+    }
+}
