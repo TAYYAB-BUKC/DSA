40d4326da5906b7f3f64d551eff5108ccd9d59ef
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-09 23:53:46 +0500

    Testing method implemented in Fibonacci in FibonacciProject.

--- Basics/Recursion/DSA.Basics.Fibonacci/Program.cs
+++ Basics/Recursion/DSA.Basics.Fibonacci/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.Fibonacci;
+
+Console.WriteLine("Enter Number : ");
+int terms = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= terms; i++)
+    Console.Write(Fibonacci.CalculateFibonacciSeries(i) + " ");
