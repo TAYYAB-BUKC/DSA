9995beb9e83baccb6fbe9745cd53be7cf0bc4211
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-10 11:15:05 +0500

    Testing method implemented in Program in TowerofHanoiProject.

--- Basics/Recursion/DSA.Basics.TowerofHanoi/Program.cs
+++ Basics/Recursion/DSA.Basics.TowerofHanoi/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.TowerofHanoi;
+
+Console.Write("Enter the number of disks : ");
+int number = Convert.ToInt32(Console.ReadLine());
+Hanoi.TowerofHanoi(number, 'A', 'B', 'C');
+
+Console.ReadKey();
