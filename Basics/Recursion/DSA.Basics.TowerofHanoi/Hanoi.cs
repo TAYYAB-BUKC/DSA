1ba69784466578170856cfc7bf9c49a60285c8b2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-10 11:12:17 +0500

    TowerofHanoi method implemented in Hanoi in TowerofHanoiProject.

---
+++ Basics/Recursion/DSA.Basics.TowerofHanoi/Hanoi.cs
@@ -0,0 +1,18 @@
+﻿namespace DSA.Basics.TowerofHanoi
+{
+    public class Hanoi
+    {
+        public static void TowerofHanoi(int number, char source, char temp, char dest)
+        {
+            if (number == 1)
+            {
+                Console.WriteLine("Move Disk " + number + " from " + source + "-->" + dest);
+                return;
+            }
+
+            TowerofHanoi(number - 1, source, dest, temp);
+            Console.WriteLine("Move Disk " + number + " from " + source + "-->" + dest);
+            TowerofHanoi(number - 1, temp, source, dest);
+        }
+    }
+}
