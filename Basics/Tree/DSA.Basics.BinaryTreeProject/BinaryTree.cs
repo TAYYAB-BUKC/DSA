3da95673f642e2bb5387796d5cd12352d3d92dd2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-11 22:17:47 +0500

    CreateDummyTree method implemented in BinaryTree in BinaryTreeProject.

---
+++ Basics/Tree/DSA.Basics.BinaryTreeProject/BinaryTree.cs
@@ -0,0 +1,17 @@
+﻿namespace DSA.Basics.BinaryTreeProject
+{
+    public class BinaryTree
+    {
+        private Node root = null!;
+
+        public void CreateDummyTree()
+        {
+            root = new Node('P');
+            root.leftChild = new Node('Q');
+            root.rightChild = new Node('R');
+            root.leftChild.leftChild = new Node('A');
+            root.leftChild.rightChild = new Node('B');
+            root.rightChild.leftChild = new Node('X');
+        }
+    }
+}
