60b89dd72254f2ec2f0dcaa98631e39cb981f34e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-15 22:57:45 +0500

    IsEmpty method implemented in BinarySearchTreeProject in BinarySearchTreeProject.

---
+++ Basics/Tree/DSA.Basics.BinarySearchTreeProject/BinarySearchTree.cs
@@ -0,0 +1,12 @@
+﻿namespace DSA.Basics.BinarySearchTreeProject
+{
+	public class BinarySearchTree
+	{
+		private Node root = null!;
+
+		public bool IsEmpty()
+		{
+			return root is null;
+		}
+	}
+}
