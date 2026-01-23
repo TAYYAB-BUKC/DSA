82096dc70839d585f715e30a0d3fcaf0eb1a6387
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-23 23:44:20 +0500

    User Menu Added.

--- Basics/Tree/DSA.Basics.BinarySearchTreeProject/Program.cs
+++ Basics/Tree/DSA.Basics.BinarySearchTreeProject/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.BinarySearchTreeProject;
+
+BinarySearchTree tree = new();
+int choice, info;
+
+while (true)
+{
+	Console.WriteLine("1. Display Tree");
+	Console.WriteLine("2. Search");
+	Console.WriteLine("3. Insert a new node");
+	Console.WriteLine("4. Delete a node");
+	Console.WriteLine("5. Preorder Traversal");
+	Console.WriteLine("6. Inorder Traversal");
+	Console.WriteLine("7. Postorder Traversal");
+	Console.WriteLine("8. Height of tree");
+	Console.WriteLine("9. Find Minimum key");
+	Console.WriteLine("10. Find Maximum key");
+	Console.WriteLine("11. Quit");
+	Console.Write("Enter your choice : ");
+	choice = Convert.ToInt32(Console.ReadLine());
+
+	if (choice == 11)
+		break;
+
+	switch (choice)
+	{
+		default:
+			Console.WriteLine("Wrong choice");
+			break;
+	}
+}
