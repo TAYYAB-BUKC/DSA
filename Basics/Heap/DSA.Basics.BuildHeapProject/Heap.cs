4c37c3d67eceb537a10346909c41c76bda99be1b
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-26 23:16:52 +0500

    RestoreUp method implemented in Heap in BuildHeapProject.

---
+++ Basics/Heap/DSA.Basics.BuildHeapProject/Heap.cs
@@ -0,0 +1,19 @@
+﻿namespace DSA.Basics.BuildHeapProject
+{
+	public class Heap
+	{
+		private void RestoreUp(int size, int[] heap)
+		{
+			int value = heap[size];
+			int parentIndex = size / 2;
+
+			while (heap[parentIndex] < value)
+			{
+				heap[size] = heap[parentIndex];
+				size = parentIndex;
+				parentIndex = size / 2;
+			}
+			heap[size] = value;
+		}
+	}
+}
