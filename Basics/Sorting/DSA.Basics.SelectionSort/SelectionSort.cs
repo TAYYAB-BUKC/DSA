b77329c13ed9fef7f07814e699064f36306acd9d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-28 23:23:17 +0500

    Sort method implemented in SelectionSort.

---
+++ Basics/Sorting/DSA.Basics.SelectionSort/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿namespace DSA.Basics.SelectionSort
+{
+	public class SelectionSort
+	{
+		public static void Sort(int[] array, int length)
+		{
+			int minIndex, tempIndex, index, secondIndex;
+
+			for (index = 0; index < length - 1; index++)
+			{
+				minIndex = index;
+				for (secondIndex = index + 1; secondIndex < length; secondIndex++)
+				{
+					if (array[secondIndex] < array[minIndex])
+						minIndex = secondIndex;
+				}
+				if (index != minIndex)
+				{
+					tempIndex = array[index];
+					array[index] = array[minIndex];
+					array[minIndex] = tempIndex;
+				}
+			}
+		}
+	}
+}
