b6df78b5e310f59700b506793f28e565c692e1bc
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-28 23:49:06 +0500

    Testing method implemented in Program in BubbleSort.

--- Basics/Sorting/DSA.Basics.BubbleSort/Program.cs
+++ Basics/Sorting/DSA.Basics.BubbleSort/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.BubbleSort;
+
+int index, numberOfElements;
+
+Console.Write("Enter the number of elements : ");
+numberOfElements = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[numberOfElements];
+
+for (index = 0; index < numberOfElements; index++)
+{
+	Console.Write("Enter element " + (index + 1) + " : ");
+	array[index] = Convert.ToInt32(Console.ReadLine());
+}
+
+BubbleSort.Sort(array, numberOfElements);
+
+Console.WriteLine("Sorted array is : ");
+for (index = 0; index < numberOfElements; index++)
+	Console.Write(array[index] + " ");
+Console.WriteLine();
