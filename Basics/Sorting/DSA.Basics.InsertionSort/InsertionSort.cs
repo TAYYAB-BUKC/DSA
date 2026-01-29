6621b7a58a965508552f03e7c4c2a433776cead1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-29 23:24:30 +0500

    Sort method implemented in InsertionSort.

---
+++ Basics/Sorting/DSA.Basics.InsertionSort/InsertionSort.cs
@@ -0,0 +1,19 @@
+﻿namespace DSA.Basics.InsertionSort
+{
+	public class InsertionSort
+	{
+		public static void Sort(int[] array, int length)
+		{
+			int index, secondIndex, tempIndex;
+			for (index = 1; index < length; index++)
+			{
+				tempIndex = array[index];
+
+				for (secondIndex = index - 1; secondIndex >= 0 && array[secondIndex] > tempIndex; secondIndex--)
+					array[secondIndex + 1] = array[secondIndex];
+
+				array[secondIndex + 1] = tempIndex;
+			}
+		}
+	}
+}
