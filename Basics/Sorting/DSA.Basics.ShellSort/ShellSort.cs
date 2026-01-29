7250546217b3c6a4751e2a5d7ff1e32a7b3d454c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-29 23:40:29 +0500

    Sort method implemented in ShellSort.

---
+++ Basics/Sorting/DSA.Basics.ShellSort/ShellSort.cs
@@ -0,0 +1,25 @@
+﻿namespace DSA.Basics.ShellSort
+{
+	public class ShellSort
+	{
+		public static void Sort(int[] array, int length)
+		{
+			int index, secondIndex, tempIndex, value;
+
+			Console.Write("Enter maximum increment(odd value) : ");
+			value = Convert.ToInt32(Console.ReadLine());
+
+			while (value >= 1)
+			{
+				for (index = value; index < length; index++)
+				{
+					tempIndex = array[index];
+					for (secondIndex = index - value; secondIndex >= 0 && array[secondIndex] > tempIndex; secondIndex = secondIndex - value)
+						array[secondIndex + value] = array[secondIndex];
+					array[secondIndex + value] = tempIndex;
+				}
+				value = value - 2;
+			}
+		}
+	}
+}
