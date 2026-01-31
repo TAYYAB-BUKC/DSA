ada609301cb5013166f273ca5de303a1cb4d77ac
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-31 22:14:25 +0500

    Copy method implemented in MergeSort in MergeSortRecursive.

---
+++ Basics/Sorting/DSA.Basics.MergeSortRecursive/MergeSort.cs
@@ -0,0 +1,11 @@
+﻿namespace DSA.Basics.MergeSortRecursive
+{
+	public class MergeSort
+	{
+		private static void Copy(int[] finalArray, int[] array, int lowerBound, int upperBound)
+		{
+			for (int index = lowerBound; index <= upperBound; index++)
+				finalArray[index] = array[index];
+		}
+	}
+}
