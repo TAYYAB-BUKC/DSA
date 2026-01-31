310d85d07b02f1599685cd160f480ad6b54ec773
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-01-31 22:04:23 +0500

    Testing method implemented in Program in MergeSortSubArray.

--- Basics/Sorting/DSA.Basics.MergeSortSubArray/Program.cs
+++ Basics/Sorting/DSA.Basics.MergeSortSubArray/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.MergeSortSubArray;
+
+int index;
+int[] array = { 1, 3, 5, 7, 2, 4, 6, 9, 11, 14 };
+int[] temp = new int[20];
+
+MergeSort.MergeSubArray(array, temp, 0, 3, 4, 9);
+
+Console.WriteLine("Array is : ");
+for (index = 0; index <= 9; index++)
+	Console.Write(array[index] + " ");
+Console.WriteLine();
+
+Console.WriteLine("Merged array is : ");
+for (index = 0; index <= 9; index++)
+	Console.Write(temp[index] + " ");
+Console.WriteLine();
