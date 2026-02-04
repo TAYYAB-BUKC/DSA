9abd49a061904815f7c9ac5af429a339b88123bb
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-04 23:41:42 +0500

    Testing method implemented in Program in MergeSortIterative.

--- Basics/Sorting/DSA.Basics.MergeSortIterative/Program.cs
+++ Basics/Sorting/DSA.Basics.MergeSortIterative/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.MergeSortIterative;
+
+int index, arrayLength;
+
+Console.Write("Enter the number of elements : ");
+arrayLength = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[arrayLength];
+
+for (index = 0; index < arrayLength; index++)
+{
+	Console.Write("Enter element " + (index + 1) + " : ");
+	array[index] = Convert.ToInt32(Console.ReadLine());
+}
+
+MergeSort.Sort(array, arrayLength);
+
+Console.WriteLine("Sorted Array : ");
+for (index = 0; index < arrayLength; index++)
+	Console.Write(array[index] + " ");
+
+Console.WriteLine();
