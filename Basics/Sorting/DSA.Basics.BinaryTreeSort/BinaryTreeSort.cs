fa523b4d587670730814d42a0e9e3c8014c76a2f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-07 20:36:19 +0500

    IsEmpty method implemented in BinaryTreeSort in BinaryTreeSort.

---
+++ Basics/Sorting/DSA.Basics.BinaryTreeSort/BinaryTreeSort.cs
@@ -0,0 +1,13 @@
+﻿namespace DSA.Basics.BinaryTreeSort
+{
+	public class BinaryTreeSort
+	{
+		private Node root = null!;
+		private static int k;
+
+		public bool IsEmpty()
+		{
+			return root is null;
+		}
+	}
+}
