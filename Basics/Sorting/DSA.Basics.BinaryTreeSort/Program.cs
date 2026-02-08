03d5d75f86fd21e2d6df77859e3c5cdbeb323eb0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-08 23:48:52 +0500

    Testing method implemented in Program in BinaryTreeSort.

--- Basics/Sorting/DSA.Basics.BinaryTreeSort/Program.cs
+++ Basics/Sorting/DSA.Basics.BinaryTreeSort/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.BinaryTreeSort;
+
+int index, length;
+
+Console.Write("Enter the number of elements : ");
+length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[length];
+
+for (index = 0; index < length; index++)
+{
+	Console.Write("Enter element " + (index + 1) + " : ");
+	array[index] = Convert.ToInt32(Console.ReadLine());
+}
+
+BinaryTreeSort.Sort(array, length);
+
+Console.WriteLine("Sorted Array : ");
+for (index = 0; index < length; index++)
+	Console.Write(array[index] + " ");
+
+Console.WriteLine();
