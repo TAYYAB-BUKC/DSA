346d8a049132873d3a765ce7a9210b9ad8ac9784
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-09 23:57:48 +0500

    RestoreDown method implemented in HeapSort in HeapSort.

---
+++ Basics/Sorting/DSA.Basics.HeapSort/HeapSort.cs
@@ -0,0 +1,39 @@
+﻿namespace DSA.Basics.HeapSort
+{
+	public class HeapSort
+	{
+		private static void RestoreDown(int index, int[] heap, int size)
+		{
+			int value = heap[index];
+			int leftChild = 2 * index, rightChild = leftChild + 1;
+
+			while (rightChild <= size)
+			{
+				if (value >= heap[leftChild] && value >= heap[rightChild])
+				{
+					heap[index] = value;
+					return;
+				}
+				else if (heap[leftChild] > heap[rightChild])
+				{
+					heap[index] = heap[leftChild];
+					index = leftChild;
+				}
+				else
+				{
+					heap[index] = heap[rightChild];
+					index = rightChild;
+				}
+				leftChild = 2 * index;
+				rightChild = leftChild + 1;
+			}
+
+			if (leftChild == size && value < heap[leftChild])
+			{
+				heap[index] = heap[leftChild];
+				index = leftChild;
+			}
+			heap[index] = value;
+		}
+	}
+}
