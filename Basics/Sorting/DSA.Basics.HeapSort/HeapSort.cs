cbf23a9379615aec80cbda7304a2543334c5ac85
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-09 23:58:23 +0500

    BuildHeap_BottomUp method implemented in HeapSort in HeapSort.

--- Basics/Sorting/DSA.Basics.HeapSort/HeapSort.cs
+++ Basics/Sorting/DSA.Basics.HeapSort/HeapSort.cs
@@ -35,5 +35,11 @@
 			}
 			heap[index] = value;
 		}
+
+		public static void BuildHeap_BottomUp(int[] heap, int size)
+		{
+			for (int index = size / 2; index >= 1; index--)
+				RestoreDown(index, heap, size);
+		}
 	}
 }
