26223440119d27cbf13d5c606e6c64a9aa9d79c9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-11 23:12:03 +0500

    Digit method implemented in RadixSort in RadixSort.

---
+++ Basics/Sorting/DSA.Basics.RadixSort/RadixSort.cs
@@ -0,0 +1,17 @@
+﻿namespace DSA.Basics.RadixSort
+{
+	public class RadixSort
+	{
+		/*Returns kth digit from right in the number*/
+		public static int Digit(int info, int postition)
+		{
+			int digit = 0, index;
+			for (index = 1; index <= postition; index++)
+			{
+				digit = info % 10;
+				info /= 10;
+			}
+			return digit;
+		}
+	}
+}
