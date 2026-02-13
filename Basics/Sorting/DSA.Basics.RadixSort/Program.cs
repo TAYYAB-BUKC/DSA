edd2e256c94efbc21f01d1ccf25447fe5b0e5ae5
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-13 23:58:28 +0500

    Testing method implemented in Program in RadixSort.

--- Basics/Sorting/DSA.Basics.RadixSort/Program.cs
+++ Basics/Sorting/DSA.Basics.RadixSort/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.RadixSort;
+
+Node temp, node, start = null!;
+int index, length, info;
+
+Console.WriteLine("Enter number of elements in the list : ");
+length = Convert.ToInt32(Console.ReadLine());
+
+for (index = 1; index <= length; index++)  /*Inserting elements in linked list*/
+{
+	Console.Write("Enter element " + index + " : ");
+	info = Convert.ToInt32(Console.ReadLine());
+
+	temp = new Node(info);
+	if (start == null)
+		start = temp;
+	else
+	{
+		node = start;
+		while (node.link != null)
+			node = node.link;
+		node.link = temp;
+	}
+}
+
+start = RadixSort.Sort(start, length);
+
+Console.WriteLine("Sorted list is : ");
+node = start;
+while (node != null)
+{
+	Console.Write(node.info + " ");
+	node = node.link;
+}
+Console.WriteLine();
+Console.ReadKey();
