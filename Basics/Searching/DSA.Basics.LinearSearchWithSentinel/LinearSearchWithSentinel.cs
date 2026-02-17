56f185b74538ba63fc9f5c194340a9f2d3eec06e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-18 02:58:04 +0500

    Search method implemented in LinearSearchWithSentinel in LinearSearchWithSentinel.

---
+++ Basics/Searching/DSA.Basics.LinearSearchWithSentinel/LinearSearchWithSentinel.cs
@@ -0,0 +1,19 @@
+﻿namespace DSA.Basics.LinearSearchWithSentinel
+{
+	public class LinearSearchWithSentinel
+	{
+		public static int Search(int[] array, int length, int searchValue)
+		{
+			array[length] = searchValue;
+
+			int index = 0;
+			while (searchValue != array[index])
+				index++;
+
+			if (index < length)
+				return index;
+			else
+				return -1;
+		}
+	}
+}
