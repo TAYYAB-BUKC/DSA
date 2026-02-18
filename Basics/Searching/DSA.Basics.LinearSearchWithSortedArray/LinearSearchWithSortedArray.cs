6f3a3e7d87b58bdace0e52735fb5878e7d79d0c6
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-19 04:00:08 +0500

    Search method implemented in LinearSearchWithSortedArray in LinearSearchWithSortedArray.

---
+++ Basics/Searching/DSA.Basics.LinearSearchWithSortedArray/LinearSearchWithSortedArray.cs
@@ -0,0 +1,20 @@
+﻿namespace DSA.Basics.LinearSearchWithSortedArray
+{
+	public class LinearSearchWithSortedArray
+	{
+		public static int Search(int[] array, int length, int searchValue)
+		{
+			int index;
+			for (index = 0; index < length; index++)
+			{
+				if (array[index] >= searchValue)
+					break;
+			}
+
+			if (array[index] == searchValue)
+				return index;
+			else
+				return -1;
+		}
+	}
+}
