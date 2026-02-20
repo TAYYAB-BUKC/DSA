b989075a1f301206543d89de44d77d2b173f9135
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-21 04:01:45 +0500

    Search method implemented in BinarySearchRecursive in BinarySearchRecursive.

---
+++ Basics/Searching/DSA.Basics.BinarySearchRecursive/BinarySearchRecursive.cs
@@ -0,0 +1,25 @@
+﻿namespace DSA.Basics.BinarySearchRecursive
+{
+	public class BinarySearchRecursive
+	{
+		public static int Search(int[] array, int target)
+		{
+			return Search(array, target, 0, array.Length - 1);
+		}
+
+		private static int Search(int[] array, int target, int first, int last)
+		{
+			if (first > last)
+				return -1;
+
+			int mid = (last + first) / 2;
+			
+			if (array[mid] == target)
+				return mid;
+			else if (array[mid] > target)
+				return Search(array, target, first, mid - 1);
+			else
+				return Search(array, target, mid + 1, last);
+		}
+	}
+}
