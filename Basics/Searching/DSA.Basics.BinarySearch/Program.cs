a3730027d9de63953f961bd4c671cda68efa03ff
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-20 23:52:18 +0500

    Testing method implemented in Program in BinarySearch.

--- Basics/Searching/DSA.Basics.BinarySearch/Program.cs
+++ Basics/Searching/DSA.Basics.BinarySearch/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSA.Basics.BinarySearch;
+
+int index, length, searchValue, searchValueIndex;
+
+Console.Write("Enter the number of elements : ");
+length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[length];
+
+Console.WriteLine("Enter the elements in sorted order - ");
+for (index = 0; index < length; index++)
+	array[index] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the search value : ");
+searchValue = Convert.ToInt32(Console.ReadLine());
+
+searchValueIndex = BinarySearch.Search(array, searchValue);
+
+if (searchValueIndex == -1)
+	Console.WriteLine("Value " + searchValue + " not present in the array");
+else
+	Console.WriteLine("Value " + searchValue + " present at index " + searchValueIndex);
