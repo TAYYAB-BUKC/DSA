e70d153ce0e7cb527bc64bd5f5882a4135bb148a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-28 04:22:15 +0500

    Node Class Added.

---
+++ Basics/Hashing/DSA.Basics.SeparateChaining/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace DSA.Basics.SeparateChaining
+{
+	public class Node
+	{
+		public Student info;
+		public Node link;
+
+		public Node(Student info)
+		{
+			this.info = info;
+			link = null!;
+		}
+	}
+}
