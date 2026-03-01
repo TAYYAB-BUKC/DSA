68b894819b6cfa3fd9954a82599ff5acfb51cf21
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-01 22:09:46 +0500

    DisplayList method implemented in SingleLinkedList in SeparateChaining.

---
+++ Basics/Hashing/DSA.Basics.SeparateChaining/SingleLinkedList.cs
@@ -0,0 +1,25 @@
+﻿namespace DSA.Basics.SeparateChaining
+{
+	public class SingleLinkedList
+	{
+		private Node start = default!;
+
+		public void DisplayList()
+		{
+			if (start is null)
+			{
+				Console.WriteLine("LinkedList is empty.");
+				return;
+			}
+
+			Console.WriteLine("LinkedList Items");
+			Node node = start;
+			while (node != null)
+			{
+				Console.Write($"{node.info}\t");
+				node = node.link;
+			}
+			Console.WriteLine();
+		}
+	}
+}
