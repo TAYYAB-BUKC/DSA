b813bcfb974e4719bbd9bf0be294e38567061fc4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-22 19:46:40 +0500

    GenerateHash method implemented in SeparateChaining.

---
+++ Basics/Hashing/DSA.Basics.SeparateChaining/HashTable.cs
@@ -0,0 +1,25 @@
+﻿namespace DSA.Basics.SeparateChaining
+{
+	public class HashTable
+	{
+		private SingleLinkedList[] array;
+		private int size;
+		private int length;
+
+		public HashTable() : this(11)
+		{
+		}
+
+		public HashTable(int tableSize)
+		{
+			size = tableSize;
+			length = 0;
+			array = new SingleLinkedList[tableSize];
+		}
+
+		private int GenerateHash(int key)
+		{
+			return (key % size);
+		}
+	}
+}
